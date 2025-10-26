b415e41c4c265986f93f22d41eea5a5dd8ba72d7
Gurdeep Singh <155575643+Gurdeepsingh-10@users.noreply.github.com>
2025-10-26 13:21:58 +0530

    file structrure

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="bg-gray-900 text-white h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Welcome to Music Recommender</h1>
+    </div>
+  );
+}
+
+export default App;
