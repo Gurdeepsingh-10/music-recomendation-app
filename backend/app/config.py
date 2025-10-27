a53207968cf04edd1617b8f341be6025324c946e
Gurdeep Singh <155575643+Gurdeepsingh-10@users.noreply.github.com>
2025-10-28 03:09:17 +0530

    added spotify id and secret

--- backend/app/config.py
+++ backend/app/config.py
@@ -16,10 +16,20 @@ class Settings(BaseSettings):
     JWT_SECRET_KEY: str = os.getenv("JWT_SECRET_KEY", "")
     JWT_ALGORITHM: str = os.getenv("JWT_ALGORITHM", "HS256")
     ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
+
+    # Supabase
+    SUPABASE_URL: str = os.getenv("SUPABASE_URL", "")
+    SUPABASE_KEY: str = os.getenv("SUPABASE_KEY", "")
+    POSTGRES_URL: str = os.getenv("POSTGRES_URL", "")
+    
     
     # App
     ENVIRONMENT: str = os.getenv("ENVIRONMENT", "development")
     BACKEND_CORS_ORIGINS: List[str] = ["http://localhost:5173", "http://localhost:3000"]
+    
+    # Spotify API
+    SPOTIFY_CLIENT_ID: str = os.getenv("SPOTIFY_CLIENT_ID", "")
+    SPOTIFY_CLIENT_SECRET: str = os.getenv("SPOTIFY_CLIENT_SECRET", "")
     
     class Config:
         env_file = ".env"
