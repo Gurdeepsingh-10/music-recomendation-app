2a495621e4291d12381b758c1d9a66c5a7d2cc70
Gurdeep Singh <155575643+Gurdeepsingh-10@users.noreply.github.com>
2025-10-27 22:17:46 +0530

    packages and req changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,5 @@
-import { StrictMode } from 'react'
+@"
+  import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -8,3 +9,4 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+"@ | Out-File -FilePath "src\main.jsx" -Encoding UTF8
